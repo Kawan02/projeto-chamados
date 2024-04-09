2d315bc348e9af5458a66c0d024c642a0e0904ac
Kawan02 <kawanng39@gmail.com>
2024-04-09 20:49:20 -0300

    Cadastrando usuário

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,14 +1,46 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState, createContext, useEffect } from "react";
+import { auth, database } from "../services/firebaseConnection";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
 
   function signIn(email, password) {
     console.log(email);
     console.log(password);
     alert("Logado");
+  }
+
+  // Cadastra um usuário
+  async function signUp(email, password, name) {
+    setLoadingAuth(true);
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+        await setDoc(doc(database, "users", uid), {
+          nome: name,
+          avatarUrl: null,
+        }).then(() => {
+          let data = {
+            uid: uid,
+            nome: name,
+            email: value.user.email,
+            avatarUrl: null,
+          };
+
+          setUser(data);
+          setLoadingAuth(false);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoadingAuth(false);
+      });
   }
   return (
     <AuthContext.Provider
@@ -16,6 +48,8 @@ function AuthProvider({ children }) {
         signed: !!user,
         user,
         signIn,
+        signUp,
+        loadingAuth,
       }}
     >
       {children}
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/auth";
 
 export default function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { signUp, loadingAuth } = useContext(AuthContext);
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
     if (name !== "" && email !== "" && password !== "") {
-      alert("Fazer cadastro");
+      await signUp(email, password, name);
     }
   }
 
@@ -28,7 +30,7 @@ export default function SignUp() {
           <input type="email" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
           <input type="password" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-          <button type="submit">Cadastrar</button>
+          <button type="submit"> {loadingAuth ? "Carregando..." : "Cadastrar"}</button>
         </form>
         <Link to="/">Já possui uma conta? Faça login</Link>
       </div>
